8d9b6e59dc18ed01e4b75f0ac8002f8fbb2d069e
AnilHasda <hasdaanil098@gmail.com>
2024-12-09 13:20:36 +0545

    added missing files again

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -4,14 +4,17 @@ import { fileURLToPath } from "url";
 // Resolve __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const multerConfiguration=(folderName)=>{
 let storage=multer.diskStorage({
   destination:function (req,file,cb){
     console.log(file)
-    cb(null,path.join(__dirname,"../profileImages"));
+    cb(null,path.join(__dirname,`../${folderName}`));
   },
   filename:function(req,file,cb){
     cb(null,Date.now()+"_"+file.originalname);
   }
 });
-let uploadProfile=multer({storage});
-export default uploadProfile;
+let multerImage=multer({storage});
+return multerImage;
+}
+export default multerConfiguration;
